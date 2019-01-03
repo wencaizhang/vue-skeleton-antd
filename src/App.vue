4107bc370354a2ebad90b979056d7d6ef571c44d
zhangwencai <zhangwencai@urplus.cn>
2019-01-03 15:14:30 +0800

    :sparkles:

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Skeleton :loading="loading" />
+  </div>
+</template>
+
+<script>
+import Skeleton from './components/Skeleton.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      loading: true,
+    }
+  },
+  components: {
+    Skeleton
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Skeleton.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="vue-skeleton" v-show="loading">
+    <!-- <h3  v-show="title" class="vue-skeleton-title vue-skeleton-item"></h3> -->
+    <div class="vue-skeleton-content">
+      <div class="vue-skeleton-item" v-for="i in rows" :key="i"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      required: true,
+      default: false
+    },
+    rows: {
+      type: Number,
+      default: 3
+    },
+    active: {
+      type: Boolean,
+      default: true
+    },
+    title: {
+      type: Boolean,
+      default: true
+    },
+  },
+  name: "",
+  components: {},
+  computed: {},
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+.vue-skeleton-item {
+  height: 16px;
+  margin-top: 16px;
+  background: #f2f2f2;
+  list-style: none;
+  background: linear-gradient(90deg, #f2f2f2 25%, #e6e6e6 37%, #f2f2f2 63%);
+  -webkit-animation: skeleton-loading 1.4s ease infinite;
+  animation: skeleton-loading 1.4s ease infinite;
+  background-size: 400% 100%;
+}
+.vue-skeleton-item:first-child {
+  margin: 0;
+}
+.vue-skeleton-item:last-child {
+  width: 61%;
+}
+
+@-webkit-keyframes skeleton-loading {
+  0%,
+  100% {
+    background-position: 0 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+}
+@keyframes skeleton-loading {
+  0%,
+  100% {
+    background-position: 0 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+}
+</style>
