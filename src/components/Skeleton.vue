4a401663f676ce9a3b18d3194df7bde004037ad2
zhangwencai <zhangwencai@urplus.cn>
2019-01-03 15:24:20 +0800

    增加头像占位图

--- src/components/Skeleton.vue
+++ src/components/Skeleton.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="vue-skeleton" v-show="loading">
     <!-- <h3  v-show="title" class="vue-skeleton-title vue-skeleton-item"></h3> -->
+    <div v-show="avatar" class="vue-skeleton-header">
+      <span class="vue-skeleton-avatar"></span>
+    </div>
     <div class="vue-skeleton-content">
       <div class="vue-skeleton-item" v-for="i in rows" :key="i"></div>
     </div>
@@ -27,23 +30,42 @@ export default {
       type: Boolean,
       default: true
     },
+    avatar: {
+      type: Boolean,
+      default: true
+    },
   },
   name: "",
-  components: {},
-  computed: {},
-  data() {
-    return {};
-  },
-  methods: {}
 };
 </script>
 
 <style>
+.vue-skeleton {
+  display: flex;
+  flex-direction: row;
+}
+.vue-skeleton-header {
+  padding-right: 15px;
+}
+.vue-skeleton-content {
+  flex: 1;
+}
+.vue-skeleton-avatar {
+  display: inline-block;
+  height: 40px;
+  width: 40px;
+  line-height: 40px;
+  border-radius: 50%;
+}
+
 .vue-skeleton-item {
   height: 16px;
   margin-top: 16px;
   background: #f2f2f2;
   list-style: none;
+}
+.vue-skeleton-avatar,
+.vue-skeleton-item {
   background: linear-gradient(90deg, #f2f2f2 25%, #e6e6e6 37%, #f2f2f2 63%);
   -webkit-animation: skeleton-loading 1.4s ease infinite;
   animation: skeleton-loading 1.4s ease infinite;
